1dcd166c5b7230d043b35eaf0e459b97f6adff72
root <root@localhost.localdomain>
2016-09-05 11:45:58 -0300

    Se corrige ejercicio 3

--- clase2/ejercicio3.js
+++ clase2/ejercicio3.js
@@ -7,13 +7,15 @@ var comentarios = [];
 request(url, function (err, response, body) {
 	if (!err && response.statusCode == 200) {
 		comentarios = JSON.parse(body);	
+
+		var palabras_porComentario = comentarios.map(function(u) {
+			return u.body.length;
+		});
+
+		var total_palabras = palabras_porComentario.reduce(function (val_ac, val) {return val_ac + val;}, 0);
+		var prom_palabras = total_palabras / comentarios.length;
+		console.log("Cantidad promedio de palabras por comentario: " + prom_palabras);
 	}
 });
 
-var palabras_porComentario = comentarios.map(function(u) {
-	return u.body.length;
-});
 
-var total_palabras = palabras_porComentario.reduce(function (val_ac, val) {return val_ac + val;}, 0);
-var prom_palabras = total_palabras / comentarios.length;
-console.log("Cantidad promedio de palabras por comentario: " + prom_palabras);
