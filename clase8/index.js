489218f5898e8b9e008082c5bf2e609397150077
Seba93 <root@localhost.localdomain>
2016-11-12 16:41:11 -0300

    Se agregan avances de clase 8.

--- clase8/index.js
+++ clase8/index.js
@@ -1,25 +1,24 @@
-// Server
+//Implementacion de servidor
+
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var path = require('path');
+
+app.use('/node_modules', express.static(path.join(__dirname + '/node_modules')));
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000);
 
-io.on('connection', function (socket) {
-    console.log(`${socket.id} se ha conectado`);
-    socket.on('msg:new', function(data) {
-        console.log(data);
-
-        //enviar mensajes al resto de los usuarios (broadcast)
-	io.on('connection', function (socket) {
-  		socket.broadcast.emit('user connected');
-	});
-
-    });
-
+var chat = io
+    .of('chat')
+    .on('connection', function (socket) {
+        console.log(`${socket.id} se ha conectado`);
+        socket.on('msg:new', function(data) {
+            chat.emit('msg:created', data);
+        });
 });
