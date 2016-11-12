72a80959bf17844162cc1dc4353f4ac53e69cdbf
Seba93 <root@localhost.localdomain>
2016-11-12 14:15:23 -0300

    Se agrega contenido de clase 7.

--- clase7/index.js
+++ clase7/index.js
@@ -1,23 +1,29 @@
 //Para evitar problema de "callback hell", es conveniente reemplazar http.request por promesas
 //Promesas ofrecen dos posibilidades: Reject y resolve (este ultimo se usa en caso de error)
 //Manejaremos las promesas usando bluebird
-//NOTA: Desde esta clase se comienza a utilizar yarn en vez de npm para la instalacion de depedencias, por
-//	motivos de eficiencia en dicho proceso
+//NOTA: Desde esta clase se comienza a utilizar yarn en vez de npm para la instalacion de depedencias, por motivos de eficiencia en dicho proceso
 
 const request = require('request');
 const API_URL = 'http://jsonplaceholder.typicode.com';
-const Promise = require ('bluebird');
+const Promise = require('bluebird');
+
+//Esta es la forma correcta de convertir una funcion con callback en promesas
+//var myrequest = Promise.promisify(request); 
 
 function myRequest(url) {
 	return new Promise(function (resolve, reject) {
-		request()
+		request(url, function(err, response, body) {
+			if (err) {
+				return reject(err);
+			}
+			resolve(JSON.parse(body));
+		});
 	});
 }
 
 //Procesamiento secuencial de requests (no es lo ideal)
-var promesa = myRequest(API_URL + '/users');
-	.then(function(body) {
-		console.log(body);
+var promesa = myRequest(API_URL + '/users')
+	.then(function(u) {
 		return myRequest(API_URL + '/albums')
 	})
 	.then(function (albums) {
@@ -36,6 +42,5 @@ var promesa2 = myRequest(API_URL + '/albums');
 var promesa3 = myRequest(API_URL + '/photos');
 Promise.all([promesa1, promesa2, promesa3])
 	.then(function(x) {
-		//x = [response1, response2, response3];
 		console.log(x);	
 	})
