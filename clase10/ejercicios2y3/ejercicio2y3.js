241a729e4096d8e969083ca1894faafa4d45bd0e
Seba93 <root@localhost.localdomain>
2016-11-22 00:15:47 -0300

    Se terminan ejercicios de clase 10.

--- clase10/ejercicios2y3/ejercicio2y3.js
+++ clase10/ejercicios2y3/ejercicio2y3.js
@@ -8,27 +8,13 @@ const axios = require('axios');
 const fib_func = require('./lib/fib.js');
 const API = 'https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec';
 
-//Cuando se crea la base de datos, se agrega hora por defecto
-
-var nueva_hora = {
-		hora: '0',
-		minutos: '0',
-		segundos: '0',		
-};
-
-db.horario.create(nueva_hora)
-	.then(() => {
-	})
-	.catch((err) => {
-		console.log(err);
-	});
-
 //Mostrar hora en nuestro sitio
 app.get('/guardar_hora', (req, res) => {
 		var datos = '';
 		var hora_ = '';
 		var minutos_ = '';
 		var segundos_ = '';
+
 		//Obtener hora del sitio web
 		axios.get(API)
 		.then(function (response) {
@@ -37,27 +23,53 @@ app.get('/guardar_hora', (req, res) => {
 			minutos_ = datos.minutes;
 			segundos_ = datos.seconds;
 
-			db.horario.findOne({
-				where: {
-					id: 1
-				}
-			}).then(function(h) {
-				return h;	
-			}).then(function (h) {
-				db.horario.update({hora: hora_, minutos: minutos_, segundos: segundos_}, {where: {id: 1}})
-				.then(function() {
-				});
+			db.horario.count().then(function (num_rows) {
+				
+				//Si la base de datos esta vacia, se INSERTA hora actual
+				if (num_rows == 0) {
 
-				res.json(h);
-				res.end();				
+					var nueva_hora = {
+						hora: hora_,
+						minutos: minutos_,
+						segundos: segundos_,		
+					};					
+
+					db.horario.create(nueva_hora)
+					.then((h) => {
+						res.json(h);
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+				}
+
+				//En caso contrario, se MODIFICA la hora almacenada
+				else {
+				
+					db.horario.findOne({
+						where: {
+							id: 1
+						}
+					}).then(function(h) {
+						return h;	
+					}).then(function (h) {
+						db.horario.update({hora: hora_, minutos: minutos_, segundos: segundos_}, {where: {id: 1}})
+					.then(function() {
+					});
+
+					res.json(h);
+					res.end();				
+					});	
+				}			
+
 			});
 		});	
 });
 
 //Crear nuevo endpoint GET /fibonacci/:n que muestre el n-esimo numero de fibonacci
 app.get('/fibonacci/:n', (req, res) => {
-		var fib_n = fib_func.fib(req.params.n);
-		var label = "fibonacci-" + req.params.n.toString();
+		var fib_n = fib_func.fib(parseInt(req.params.n, 10));
+		var label = "fibonacci-" + req.params.n;
 		var datos = {};
 		datos[label] = fib_n.toString();
 		res.json(datos);
